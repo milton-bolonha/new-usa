dc3d3488967f6aece92bf69cc4071d60761abdc0
Milton Bolonha <miltonsiedler@gmail.com>
2026-02-21 16:36:45 -0300

    update repo

--- netlify/functions/hello.ts
+++
@@ -1,7 +0,0 @@
-// netlify/functions/hello.ts
-export async function handler(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
-    };
-}
